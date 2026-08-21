456b9fe637bdefac33f5cdaf75e2ddcadb617020
T-Bird04 <terrence.gainesjr@gmail.com>
2026-08-20 22:10:10 -0500

    Update ArchInstallScriptSATA.bash

--- ArchInstallScriptSATA.bash
+++ ArchInstallScriptSATA.bash
@@ -32,8 +32,8 @@ mkswap /dev/sda2
 swapon /dev/sda2
 
 #Encrypt partitions
-cryptsetup luksFormat --type luks2 /dev/sda2 --Key-File 
-cryptsetup luksFormat --type luks2 /dev/sda3 --Key-File
+cryptsetup luksFormat --type luks2 /dev/sda2 --Key-File /password.txt
+cryptsetup luksFormat --type luks2 /dev/sda3 --Key-File /password.txt
 
 #Mount btrfs partition
 mount /dev/sda3 /mnt #Mount Root
