da34b6a130ab7c7a540e603a8d5396d8a2f0f167
T-Bird04 <terrence.gainesjr@gmail.com>
2026-08-24 13:25:37 -0500

    Update ArchInstallScriptFinalSATA.bash

--- ArchInstallScriptFinalSATA.bash
+++ ArchInstallScriptFinalSATA.bash
@@ -35,19 +35,6 @@ cryptsetup open --type luks2 /dev/sda3 cryptroot
 #Format boot partition
 mkfs.fat -F 32 /dev/sda1 #EFI Boot parition is FAT32
 
-#Find UUID for swap partition
-SWAP_PARTUUID=$(blkid -s PARTUUID -o value /dev/sda2)
-
-#Encrypt swap with random key
-cat > /mnt/etc/crypttab <<EOF
-swap /dev/disk/by-partuuid/${SWAP_PARTUUID} /dev/urandom swap,cipher=aes-xts-plain64,size=512,sector-size=4096
-EOF
-
-#Update fstab for swap unencryption
-cat >> /mnt/etc/fstab <<EOF
-/dev/mapper/swap none swap defaults 0 0
-EOF
-
 #Format root partition
 mkfs.btrfs -f /dev/mapper/cryptroot
 
@@ -143,6 +130,19 @@ pacstrap -K /mnt ${CORE_PACKAGES[@]} ${HARDWARE_PACKAGES[@]} ${SERVICE_PACKAGES[
 
 #Update fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
+
+#Find UUID for swap partition
+SWAP_PARTUUID=$(blkid -s PARTUUID -o value /dev/sda2)
+
+#Encrypt swap with random key
+cat > /mnt/etc/crypttab <<EOF
+swap /dev/disk/by-partuuid/${SWAP_PARTUUID} /dev/urandom swap,cipher=aes-xts-plain64,size=512,sector-size=4096
+EOF
+
+#Update fstab for swap unencryption
+cat >> /mnt/etc/fstab <<EOF
+/dev/mapper/swap none swap defaults 0 0
+EOF
 
 #Enter chroot
 arch-chroot /mnt <<CHROOT_EOF
